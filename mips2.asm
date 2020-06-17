2084f6596429af108ddc613b87a8cb58350ca2b8
bg7jws <38001684+bg7jws@users.noreply.github.com>
2020-06-17 14:29:18 +0800

    Revert "Update mips2.asm"
    This reverts commit 9c5c9c17bb3f1c632f8b3691125f7eff0ed6eaf6.

--- mips2.asm
+++ mips2.asm
@@ -3,10 +3,10 @@ main:
    la  $s1,arrayA
    lbu $s2,number
    ori  $a0,0              #mov ax,0
-   sub $s2,$s2,1           #dec cx
+   sub $s2,$s2,1       #dec cx
    loop1:
-   move $k0, $s2           #push cx
-   move $k1, $s1           #push si  
+   move $k0, $s2    #push cx
+   move $k1, $s1    #push si  
    loop2: 
    lbu $a0, ($s1)
    lbu $a1, 1($s1)
@@ -17,11 +17,11 @@ main:
    cont:
    addiu $t0,$t0,1
    sub $s2,$s2,1
-   bgtz $s2, loop2         #loop loop2
-   move $t0,$k1            #pop si
-   move $s2,$k0            #pop cx
+   bgtz $s2, loop2          #loop loop2
+   move $t0,$k1     #pop si
+   move $s2,$k0     #pop cx
    sub $s2,$s2,1
-   bgtz $s2, loop1         #loop loop1
+   bgtz $s2, loop1          #loop loop1
 .data	
 arrayA:
    .byte 45,32,16,22,9,14
