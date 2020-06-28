b58009b2298e63f5d3b0e9bcfdd93ab889530b8f
bg7jws <szdataio@163.com>
2020-06-28 18:44:45 +0800

    modify

--- addnum.asm
+++ addnum.asm
@@ -1,16 +1,16 @@
    .text
 main:
-   lw $t1,number		#initial the first number
-   lw $t4,times			#initial cycle times
+   lw $t1,number		      #initial the first number
+   lw $t4,times			   #initial cycle times
 begin:
-   addi $t3,$t3,4		#pointer to next number
+   addi $t3,$t3,4		      #pointer to next number
    lw $t2 number($t3)		#load the number to $t2
-   add $t1,$t1,$t2		#add once
-   subi $t4,$t4,1		#counter-1
-   blez $t4 finish		#less or equ 0 finish
-   j begin			#continue add
+   add $t1,$t1,$t2		   #add once
+   subi $t4,$t4,1		      #counter-1
+   blez $t4 finish		   #less or equ 0 finish
+   j begin			         #continue add
 finish:
-   add $t3,$t3,4		#pointer to the result address
+   add $t3,$t3,4		      #pointer to the result address
    sw $t1,number($t3)		#store the sum to result address
 .data  0x10010080
 number:
