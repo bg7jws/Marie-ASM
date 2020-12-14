aad8bdcf76f00dba80144ffb3f0511fa07ad1059
bg7jws <38001684+bg7jws@users.noreply.github.com>
2020-12-14 12:33:21 +0800

    Update addnum.asm

--- addnum.asm
+++ addnum.asm
@@ -1,20 +1,20 @@
    .text
 main:
-   lw $t1,number		      #initial the first number
-   lw $t4,times			   #initial cycle times
+   lw $t1,number           #initial the first number
+   lw $t4,times            #initial cycle times
 begin:
-   addi $t3,$t3,4		      #pointer to next number
-   lw $t2 number($t3)		#load the number to $t2
-   add $t1,$t1,$t2		   #add once
-   subi $t4,$t4,1		      #counter-1
-   blez $t4 finish		   #less or equ 0 finish
-   j begin			         #continue add
+   addi $t3,$t3,4          #pointer to next number
+   lw $t2 number($t3)      #load the number to $t2
+   add $t1,$t1,$t2         #add once
+   subi $t4,$t4,1          #counter-1
+   blez $t4 finish         #less or equ 0 finish
+   j begin                 #continue add
 finish:
-   add $t3,$t3,4		      #pointer to the result address
-   sw $t1,number($t3)		#store the sum to result address
+   add $t3,$t3,4           #pointer to the result address
+   sw $t1,number($t3)      #store the sum to result address
 .data  0x10010080
 number:
    .word 43218888,23459999,84563211,-500300888,0		# define the numbers at here
    .word -349058790
 times:
-   .byte 3			         # define how many numbers do add,
+   .byte 3                 # define how many numbers do add,
